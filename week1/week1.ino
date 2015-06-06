cf79dd57fcbf66602fe7238af595211507cd186f
Jonathan Whitten <jon.whitten@gmail.com>
2015-06-05 22:39:47 -0400

    Week1 commit

---
+++ week1/week1.ino
@@ -0,0 +1,41 @@
+//Author: Jonathan Whitten
+//Date: 05 June 2015
+//Title: Week1.ino
+//Description: This program acts as a brief introduction
+//  to arduino sketches and other programming facts
+
+//Lines starting with `//` are comments
+/*  
+    This is a special comment style that
+    can take up multiple lines and is commonly
+    used to write instructions of some sort
+*/   
+
+// notice that `/*` opens the comment and `*/` closes it
+    
+/*  
+    All your startup code goes here and only runs one time
+    All your setup code for pins and timers and communication
+    should go in this `block` of code
+*/
+void setup(){
+    /*  
+        Set pin mode with the following commands:
+
+        pinMode( <pin number> , <pin status>);
+        digitalWrite( <pin number> , <pin status>); 
+    */
+
+
+}
+
+/* Everything that happens repeatedly goes in here */
+void loop(){
+    /*
+        Use the following commands to blink the LED:
+        
+        digitalWrite( <pin number>, <pin status>);
+        delay( <number of milliseconds);
+
+    */
+}
